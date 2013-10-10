042c5ef2d3dcae194455fdd8ab29b4a64e1c5e94
Renato Fernandes de Queiroz <renatofq@gmail.com>
2013-10-10 15:40:54 -0300

    Buffer mock.

---
+++ include/v8/buffer.h
@@ -0,0 +1,19 @@
+#ifndef V8_BUFFER_H
+#define V8_BUFFER_H
+
+typedef struct v8_buffer_t
+{
+	void * buffer;
+	int size;
+	int pos;
+} V8Buffer;
+
+V8Buffer * v8_buffer_create(void);
+
+void v8_buffer_destroy(V8Buffer * buffer);
+
+int v8_buffer_write(V8Buffer * buffer, void * data, int size);
+
+int v8_buffer_dump(const V8Buffer * buffer, int fd);
+
+#endif
---
+++ src/buffer.c
@@ -0,0 +1,19 @@
+#include <v8/buffer.h>
+
+V8Buffer * v8_buffer_create(void)
+{
+	return NULL;
+}
+
+void v8_buffer_destroy(V8Buffer * buffer)
+{
+
+}
+
+int v8_buffer_write(V8Buffer * buffer, void * data, int size)
+{
+}
+
+int v8_buffer_dump(const V8Buffer * buffer, int fd)
+{
+}
