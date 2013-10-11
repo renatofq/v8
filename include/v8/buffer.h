9037ae7cb14a8acef4d8fe96b20bcde0594cb613
Renato Fernandes de Queiroz <renatofq@gmail.com>
2013-10-11 17:30:27 -0300

    Implementacao de  buffer.

--- include/v8/buffer.h
+++ include/v8/buffer.h
@@ -1,16 +1,13 @@
 #ifndef V8_BUFFER_H
 #define V8_BUFFER_H
 
-typedef struct v8_buffer_t
-{
-	void * buffer;
-	int size;
-	int pos;
-} V8Buffer;
+typedef struct v8_buffer_t V8Buffer;
 
 V8Buffer * v8_buffer_create(void);
 
 void v8_buffer_destroy(V8Buffer * buffer);
+
+int v8_buffer_size(const V8Buffer * buffer);
 
 int v8_buffer_write(V8Buffer * buffer, const void * data, int size);
 
--- src/buffer.c
+++ src/buffer.c
@@ -1,23 +1,116 @@
 #include <v8/buffer.h>
 
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+struct v8_buffer_t
+{
+	void * p;
+	int size;
+	int pos;
+};
+
+static const size_t V8_BUFFER_BASE_SIZE = (8*1024);
+
+static int v8_buffer_new_size(int size, int min_size);
 
 V8Buffer * v8_buffer_create(void)
 {
-	return NULL;
+	V8Buffer * buffer = (V8Buffer *)malloc(sizeof(V8Buffer));
+
+	if (buffer != NULL)
+	{
+		buffer->p = NULL;
+		buffer->size = 0;
+		buffer->pos = 0;
+	}
+
+	return buffer;
 }
 
 void v8_buffer_destroy(V8Buffer * buffer)
 {
+	if (buffer == NULL)
+	{
+		return;
+	}
 
+	if (buffer->p != NULL)
+	{
+		free(buffer->p);
+		buffer->p = NULL;
+	}
+
+	buffer->size = 0;
+	buffer->pos = -1;
+}
+
+int v8_buffer_size(const V8Buffer * buffer)
+{
+	if (buffer == NULL)
+  {
+	  return 0;
+  }
+	else
+  {
+	  return buffer->pos;
+  }
 }
 
 int v8_buffer_write(V8Buffer * buffer, const void * data, int size)
 {
-	return 0;
+
+	if (buffer == NULL || data == NULL || size <= 0)
+	{
+		return 0;
+	}
+
+	if (buffer->pos + size > buffer->size)
+  {
+	  void * ptr = NULL;
+	  int new_size = v8_buffer_new_size(buffer->size, buffer->pos + size);
+
+	  ptr = realloc(buffer->p, new_size);
+
+	  if (ptr == NULL)
+		{
+			return 0;
+		}
+	  else
+	  {
+		  buffer->p = ptr;
+		  buffer->size = new_size;
+		}
+  }
+
+	memcpy(buffer->p + buffer->pos, data, size);
+	buffer->pos += size;
+
+	return size;
 }
 
 int v8_buffer_dump(const V8Buffer * buffer, int fd)
 {
-	return 0;
+	if (buffer == NULL || fd < 0)
+	{
+		return -1;
+	}
+
+	return write(fd, buffer->p, buffer->pos);
+}
+
+static int v8_buffer_new_size(int size, int min_size)
+{
+	if (size == 0)
+	{
+		size = V8_BUFFER_BASE_SIZE;
+	}
+
+	while (size < min_size)
+	{
+		size *= 2;
+	}
+
+	return size;
 }
