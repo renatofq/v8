31672a92b8ee1ca081c31cb4414d74789d01a837
Renato Fernandes de Queiroz <renatofq@gmail.com>
2013-10-21 13:05:24 -0200

    Commit atrasado do log :(

--- src/log.c
+++ src/log.c
@@ -1,7 +1,7 @@
 #include <v8/log.h>
 #include <stdarg.h>
 #include <stdio.h>
-
+#include <string.h>
 
 static V8LogLevel g_log_level = V8_LOG_NONE;
 
@@ -53,4 +53,37 @@ V8LogLevel v8_log_level(void)
 void v8_log_level_set(V8LogLevel level)
 {
 	g_log_level = level;
+}
+
+void v8_log_level_str_set(const char * level)
+{
+	if (strcasecmp(level, "debug") == 0)
+	{
+		g_log_level = V8_LOG_DEBUG;
+	}
+	else if (strcasecmp(level, "info") == 0)
+	{
+		g_log_level = V8_LOG_INFO;
+	}
+	else if (strcasecmp(level, "warning") == 0)
+	{
+		g_log_level = V8_LOG_WARN;
+	}
+  else if (strcasecmp(level, "warn") == 0)
+  {
+	  g_log_level = V8_LOG_WARN;
+  }
+  else if (strcasecmp(level, "error") == 0)
+  {
+	  g_log_level = V8_LOG_ERROR;
+  }
+  else if (strcasecmp(level, "none") == 0)
+  {
+	  g_log_level = V8_LOG_NONE;
+  }
+  else
+  {
+	  v8_log_warn("Log level unidentified, setting to info");
+	  g_log_level = V8_LOG_INFO;
+  }
 }
